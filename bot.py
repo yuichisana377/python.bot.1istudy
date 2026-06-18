639fda95faaa4214244e7d58ea2ac8df28d5909e
yuichisana377 <yuichi_sana3770@icloud.com>
2026-06-19 08:12:19 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -580,7 +580,7 @@ async def help_command(interaction: discord.Interaction):
 
 
 scheduler.add_job(send_tomorrow_plans, "cron", hour=20, minute=0)
-scheduler.add_job(send_today_plans, "cron", hour=7, minute=50)
+scheduler.add_job(send_today_plans, "cron", hour=5, minute=30)
 scheduler.add_job(cleanup_past_plans, "cron", hour=0, minute=0)
 
 started = False
